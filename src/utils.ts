d04e4a0923e0dd1257da7cf1a862e16668f47cb3
anatolykopyl <hi@kopyl.dev>
2023-12-08 02:19:25 +0300

    Added usage example

--- src/utils.ts
+++ src/utils.ts
@@ -8,16 +8,28 @@ const isChainer = (token: string): boolean => {
   return token === '>'
 }
 
+const isPseudo = (token: string): boolean => {
+  return token.endsWith('::before') || token.endsWith('::after')
+}
+
 export const isSinglyResolvable = (selectorText: string): boolean => {
   const tokens = selectorText.split(' ')
   return tokens.every((token) => hasId(token) || isChainer(token))
 }
 
 export const createElementFromQuery = (query: string) => {
-  const elementSelectors = query.split(' ').filter(hasId)
+  // TODO: Allow creating pseudo elements without declaring host before
+  const elementSelectors = query.split(' ')
+    .filter((token) => hasId(token) && !isPseudo(token))
 
   let parent = document.body
   elementSelectors.forEach((selector) => {
+    const existingElement = parent.querySelector(selector) as HTMLElement
+    if (existingElement) {
+      parent = existingElement
+      return
+    }
+
     const element = createElement(selector)
     parent.appendChild(element)
     parent = element
